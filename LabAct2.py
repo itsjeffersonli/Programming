e3035950acb8999f4b62a81e44e3a68ff8db6dc6
run3 <53053198+itsjeffersonli@users.noreply.github.com>
2020-09-10 02:16:59 +0800

    Add files via upload

---
+++ LabAct2.py
@@ -0,0 +1,32 @@
+def binarysearch(array, x): 
+    l = 0
+    r = len(array) 
+    r = r - 2 
+    while (l <= r): 
+        m = l + ((r - 1) // 2) 
+  
+        res = (x == array[m]) 
+
+        if (res == 0): 
+            return m - 1
+
+        if (res > 0): 
+            l = m + 1
+
+        else: 
+            r = m - 1
+  
+    return -1
+
+if __name__ == "__main__": 
+  
+    array = ["Jewel", "Sam", "Cha", "Cloud", "Shine"]; 
+    x = str(input("Who Do you want to search for: " + " "))
+    result = binarysearch(array, x) 
+  
+    if (result == -1): 
+        print("Element not present") 
+    else: 
+        print("Element found at index" ,result) 
+    
+    print(array)    
