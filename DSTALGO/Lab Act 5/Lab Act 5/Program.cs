7826c160e4b58c42e8c0e30df14788f60778500b
John Jefferson Li <53053198+itsjeffersonli@users.noreply.github.com>
2020-09-30 16:12:02 +0800

    1

---
+++ DSTALGO/Lab Act 5/Lab Act 5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_Act_5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int numbers_ask = 0; numbers_ask < 5; numbers_ask++)
+            {
+                int[] numbers = new int[5];
+
+                for(int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write("Enter Number: ");
+                    numbers[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int temp;
+                    int smallest;
+                    smallest = i;
+                    for (int j = i + 1; j < numbers.Length; j++)
+                    {
+                        if (numbers[j] < numbers[smallest])
+                        {
+                            smallest = j;
+                        }
+                    }
+                    temp = numbers[smallest];
+                    numbers[smallest] = numbers[i];
+                    numbers[i] = temp;
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("Sorted Array: ");
+                foreach (int item in numbers)
+                {
+                    Console.Write("\t" + item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DSTALGO/Lab Act 6/Lab Act 6/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Lab_Act_6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Quiz
+            Console.Write("Enter Quizzes Score:" + "\n");
+            int[][] grade1 = new int[1][];
+            grade1[0] = new int[3];
+            int sum1 = 0;
+            for (int quiz = 0; quiz < grade1.Length; quiz++)
+            {
+                for (int j = 0; j < grade1[quiz].Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("Enter Quiz{0}: ", j);
+                    Console.ResetColor();
+                    grade1[quiz][j] = int.Parse(Console.ReadLine());
+                    sum1 = sum1 + grade1[quiz][j];
+                }
+                Console.WriteLine();
+            }
+
+            //Assignment
+            Console.Write("Enter Assignments Score:" + "\n");
+            int[][] grade2 = new int[1][];
+            grade2[0] = new int[2];
+            int sum2 = 0;
+            for (int assignment = 0; assignment < grade2.Length; assignment++)
+            {
+                for (int j = 0; j < grade2[assignment].Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("Enter Assignment{0}: ", j);
+                    Console.ResetColor();
+                    grade2[assignment][j] = int.Parse(Console.ReadLine());
+                    sum2 = sum2 + grade2[assignment][j];
+                }
+                Console.WriteLine();
+            }
+
+            //Exams
+            Console.Write("Enter Exams Score:" + "\n");
+            int[][] grade3 = new int[1][];
+            grade3[0] = new int[2];
+            int sum3 = 0;
+            for (int exams = 0; exams < grade3.Length; exams++)
+            {
+                for (int j = 0; j < grade3[exams].Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("Enter Exams{0}: ", j);
+                    Console.ResetColor();
+                    grade3[exams][j] = int.Parse(Console.ReadLine());
+                    sum3 = sum3 + grade3[exams][j];
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\t" + "\t" + " "+ "*** Class Standing Summary ***"+ "\n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("\t" + "\t" + " " + "Scores" + "\t"+ "\t" + "\t" +"Average" +"\n");
+            Console.ResetColor();
+
+            // Print Quizz
+            for (int quiz=0; quiz < grade1.Length; quiz++)
+            {
+                Console.Write("Quiz: "+ " " + "\t" + "\t");
+                for (int j = 0; j < grade1[quiz].Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(grade1[quiz][j] + " " + "\t");
+                    Console.ResetColor();
+                }
+                double ave1 = sum1 / 3;
+                Console.Write(ave1);
+            }
+            Console.WriteLine();
+
+            // Print Assignment
+            for (int assignment = 0; assignment < grade2.Length; assignment++)
+            {
+                Console.Write("Assignment: " + " " + "\t");
+                for (int j = 0; j < grade2[assignment].Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(grade2[assignment][j] + " " + "\t");
+                    Console.ResetColor();
+                }
+                double ave2 = sum2 / 2;
+                Console.Write("\t"+ave2);
+            }
+            Console.WriteLine();
+
+            // Print Exams
+            for (int exams = 0; exams < grade3.Length; exams++)
+            {
+                Console.Write("Exams: " + " " + "\t");
+                for (int j = 0; j < grade3[exams].Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(grade3[exams][j] + " " + "\t");
+                    Console.ResetColor();
+                }
+                double ave3 = sum3 / 2;
+                Console.Write("\t"+ave3);
+            }
+            Console.WriteLine();
+        }
+    }
+}
